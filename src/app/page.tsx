625231875080f076d3041ca8b850af1d0ddb3b6e
Jayde Mitchell <jayde@jaydemitchell.com>
2025-04-07 14:59:09 -0700

    more type error fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Link from "next/link";
 
 // import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
-import Hero from "./_components/hero";
-import ParallaxText from "./_components/ParallaxText";
+import { auth } from "~/server/auth"
+import { api, HydrateClient } from "~/trpc/server"
+import Hero from "./_components/hero"
+import ParallaxText from "./_components/ParallaxText"
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  // const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
